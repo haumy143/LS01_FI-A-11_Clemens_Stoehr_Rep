95f0ab342efd0535453d6b241b360d187e5453da
Clemens <66621176+haumy143@users.noreply.github.com>
2021-09-25 17:26:28 +0200

    Update Temperaturtabelle.java

--- Temperaturtabelle/src/Temperaturtabelle.java
+++ Temperaturtabelle/src/Temperaturtabelle.java
@@ -3,9 +3,9 @@ public class Temperaturtabelle {
         System.out.printf("%-12s|%10s%n", "Fahrenheit", "Celsius");
         System.out.printf("%s%n", "-----------------------");
         System.out.printf("%-12s| %9.2f %n", -20, -28.8889);
-        System.out.printf("%-12s| %9.2f%n", -10, -23.3333);    
-        System.out.printf("%-12s| %9.2f%n", "+0", -17.7778); 
-        System.out.printf("%-12s| %9.2f%n", "+20", -6.6667);
-        System.out.printf("%-12s| %9.2f%n", "+30", -1.1111); 
+        System.out.printf("%-12s| %9.2f %n", -10, -23.3333);    
+        System.out.printf("%-12s| %9.2f %n", "+0", -17.7778); 
+        System.out.printf("%-12s| %9.2f %n", "+20", -6.6667);
+        System.out.printf("%-12s| %9.2f %n", "+30", -1.1111); 
     }
 }
