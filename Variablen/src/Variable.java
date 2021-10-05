84e1ee320d9601d39654a571515c7e367279d81c
haumy143 <66621176+haumy143@users.noreply.github.com>
2021-10-05 12:02:05 +0200

    Created Project Variablen

---
+++ Variablen/src/Variable.java
@@ -0,0 +1,59 @@
+public class Variable {
+    public static void main(String [] args){
+        /* 1. Ein Zaehler soll die Programmdurchlaeufe zaehlen.
+              Vereinbaren Sie eine geeignete Variable */
+            int programmdurchlaeufe;
+    
+        /* 2. Weisen Sie dem Zaehler den Wert 25 zu
+              und geben Sie ihn auf dem Bildschirm aus.*/
+            programmdurchlaeufe = 25;
+            System.out.println(programmdurchlaeufe);
+        
+        /* 3. Durch die Eingabe eines Buchstabens soll der Menuepunkt
+              eines Programms ausgewaehlt werden.
+              Vereinbaren Sie eine geeignete Variable */
+            char auswahl;
+        
+        /* 4. Weisen Sie dem Buchstaben den Wert 'C' zu
+              und geben Sie ihn auf dem Bildschirm aus.*/
+            auswahl = 'C';
+            System.out.println(auswahl);
+
+        /* 5. Fuer eine genaue astronomische Berechnung sind grosse Zahlenwerte
+              notwendig.
+              Vereinbaren Sie eine geeignete Variable */
+            double astronomischeZahl;
+    
+        /* 6. Weisen Sie der Zahl den Wert der Lichtgeschwindigkeit zu
+              und geben Sie sie auf dem Bildschirm aus.*/
+            astronomischeZahl = 299792458;
+            System.out.println("Die Lichtgeschwindigkeit beträgt " + astronomischeZahl + "m/s.");
+        
+        /* 7. Sieben Personen gruenden einen Verein. Fuer eine Vereinsverwaltung
+              soll die Anzahl der Mitglieder erfasst werden.
+              Vereinbaren Sie eine geeignete Variable und initialisieren sie
+              diese sinnvoll.*/
+            byte anzahlMitglieder = 7;
+
+        /* 8. Geben Sie die Anzahl der Mitglieder auf dem Bildschirm aus.*/
+            System.out.println("Wir haben " + anzahlMitglieder + " Mitlgieder.");
+    
+        /* 9. Fuer eine Berechnung wird die elektrische Elementarladung benoetigt.
+              Vereinbaren Sie eine geeignete Variable und geben Sie sie auf
+              dem Bildschirm aus.*/
+            double elementarladung;
+        
+        /*10. Ein Buchhaltungsprogramm soll festhalten, ob eine Zahlung erfolgt ist.
+              Vereinbaren Sie eine geeignete Variable. */
+            boolean zahlungErfolgt;
+    
+        /*11. Die Zahlung ist erfolgt.
+              Weisen Sie der Variable den entsprechenden Wert zu
+              und geben Sie die Variable auf dem Bildschirm aus.*/
+            zahlungErfolgt = true;
+            if (zahlungErfolgt == true);
+                System.out.println("zahlung ist erfolgt.");
+
+      }//main
+    }// Variablen
+
---
+++ Variablen/src/Welt_der_Zahlen.java
@@ -0,0 +1,59 @@
+public class Welt_der_Zahlen {
+    public static void main(String[] args) throws Exception {
+        /*  *********************************************************
+    
+         Zuerst werden die Variablen mit den Werten festgelegt!
+    
+    *********************************************************** */
+    // Im Internet gefunden ?
+    // Die Anzahl der Planeten in unserem Sonnesystem                    
+    int anzahlPlaneten = 8 ;
+    
+    // Anzahl der Sterne in unserer Milchstraße
+    long anzahlSterne = 100000000000l;
+    
+    // Wie viele Einwohner hat Berlin?
+    int bewohnerBerlin = 3645000;
+    
+    // Wie alt bist du?  Wie viele Tage sind das?
+    
+    short alterTage = 8670;
+    
+    // Wie viel wiegt das schwerste Tier der Welt?
+    // Schreiben Sie das Gewicht in Kilogramm auf!
+    int gewichtKilogramm = 150000;
+    
+    // Schreiben Sie auf, wie viele km² das größte Land er Erde hat?
+    int flaecheGroessteLand = 17130000;
+    
+    // Wie groß ist das kleinste Land der Erde?
+    
+    float flaecheKleinsteLand = 0.44f;
+    
+    
+    
+    
+    /*  *********************************************************
+    
+         Programmieren Sie jetzt die Ausgaben der Werte in den Variablen
+    
+    *********************************************************** */
+    
+    System.out.println("Anzhahl der Planeten: " + anzahlPlaneten);
+    
+    System.out.println("Anzahl der Sterne:" + anzahlSterne);
+
+    System.out.println("Berlin hat " + bewohnerBerlin + " Einwohner.");
+
+    System.out.println("Ich bin "+ alterTage + " Tage alt.");
+
+    System.out.println("Das schwerste Tier der Welt wiegt bis zu " + gewichtKilogramm + " Kilogramm");
+
+    System.out.println("Das größte Land der Erde ist " + flaecheGroessteLand + "qm² groß.");
+
+    System.out.println("Das größte Land der Erde ist " + flaecheKleinsteLand + "qm² klein.");
+
+    
+    System.out.println(" *******  Ende des Programms  ******* ");
+    }
+}
