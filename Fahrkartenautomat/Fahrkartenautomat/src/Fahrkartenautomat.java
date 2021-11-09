234b8d2b67a3819bdda3af7d551403215f82637e
haumy143 <66621176+haumy143@users.noreply.github.com>
2021-11-09 10:26:20 +0100

    added second decimal

--- Fahrkartenautomat/Fahrkartenautomat/src/Fahrkartenautomat.java
+++ Fahrkartenautomat/Fahrkartenautomat/src/Fahrkartenautomat.java
@@ -1,14 +1,11 @@
 ﻿import java.util.Scanner;
 
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.CloseAction;
-
 class Fahrkartenautomat
 {
     public static void main(String[] args)
     {
        Scanner tastatur = new Scanner(System.in);
-       
-
+      
        double zuZahlenderBetrag; 
        double eingezahlterGesamtbetrag;
        double eingeworfeneMünze;
@@ -22,7 +19,8 @@ class Fahrkartenautomat
        eingezahlterGesamtbetrag = 0.0;
        while(eingezahlterGesamtbetrag < zuZahlenderBetrag)
        {
-    	   System.out.println("Noch zu zahlen: " + (zuZahlenderBetrag - eingezahlterGesamtbetrag));
+    	   zuZahlenderBetrag = zuZahlenderBetrag - eingezahlterGesamtbetrag;
+           System.out.printf("Noch zu zahlen: %.2f Euro\n", zuZahlenderBetrag);
     	   System.out.print("Eingabe (mind. 5Ct, höchstens 2 Euro): ");
     	   eingeworfeneMünze = tastatur.nextDouble();
            eingezahlterGesamtbetrag += eingeworfeneMünze;
@@ -48,7 +46,7 @@ class Fahrkartenautomat
        rückgabebetrag = eingezahlterGesamtbetrag - zuZahlenderBetrag;
        if(rückgabebetrag > 0.0)
        {
-    	   System.out.println("Der Rückgabebetrag in Höhe von " + rückgabebetrag + " EURO");
+    	   System.out.printf("Der Rückgabebetrag in Höhe von %.2f Euro", rückgabebetrag);
     	   System.out.println("wird in folgenden Münzen ausgezahlt:");
 
            while(rückgabebetrag >= 2.0) // 2 EURO-Münzen
