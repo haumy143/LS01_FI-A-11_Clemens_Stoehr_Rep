8c9e9a151a11f22f50e9ba3ad5c8474e62abbeac
haumy143 <66621176+haumy143@users.noreply.github.com>
2021-11-30 12:12:59 +0100

    Update Fahrkartenautomat.java
    alles mit methoden jetzt

--- Fahrkartenautomat/Fahrkartenautomat/src/Fahrkartenautomat.java
+++ Fahrkartenautomat/Fahrkartenautomat/src/Fahrkartenautomat.java
@@ -6,35 +6,58 @@ class Fahrkartenautomat
 {
     public static void main(String[] args)
     {
-        Scanner tastatur = new Scanner(System.in);
-      
         double zuZahlenderBetrag; 
         double eingezahlterGesamtbetrag;
         double eingeworfeneMünze;
         double rückgabebetrag;
         double ticketPreis;
         double anzahlTickets;
+        Scanner tastatur = new Scanner(System.in);
 
+        zuZahlenderBetrag = erfassungBestellung();
+        rückgabebetrag = bezahlvorgang(zuZahlenderBetrag);
+        fahrscheinausgabe();
+        rückgeldberechnung(rückgabebetrag);
+        guteFahrt();
+    }
+
+    
+    public static double erfassungBestellung() {
+        Scanner tastatur = new Scanner(System.in);
+        double ticketPreis;
+        double anzahlTickets;
+        double zuZahlenderBetrag;
+
+        
         System.out.print("Wie viel kostet das ein Ticket?: ");
         ticketPreis = tastatur.nextDouble();
         System.out.print("Wie viele Tickets brauchen Sie: ");
         anzahlTickets = tastatur.nextDouble();
+        zuZahlenderBetrag = ticketPreis * anzahlTickets;
+        return zuZahlenderBetrag;
+
+    } 
     
-       zuZahlenderBetrag = ticketPreis * anzahlTickets;
+    public static double bezahlvorgang(double zuZahlenderBetrag) {
+        double eingezahlterGesamtbetrag;
+        double eingeworfeneMünze;
+        double rückgabebetrag;
         
-       // Geldeinwurf
-       // -----------
+        Scanner tastatur = new Scanner(System.in);
+
         eingezahlterGesamtbetrag = 0.0;
+
         while(eingezahlterGesamtbetrag < zuZahlenderBetrag)
         {
-        System.out.printf("Noch zu zahlen: %.2f Euro\n", zuZahlenderBetrag - eingezahlterGesamtbetrag);
-    	System.out.print("Eingabe (mind. 5Ct, höchstens 2 Euro): ");
-    	eingeworfeneMünze = tastatur.nextDouble();
-        eingezahlterGesamtbetrag += eingeworfeneMünze;
+            System.out.printf("Noch zu zahlen: %.2f Euro\n", zuZahlenderBetrag - eingezahlterGesamtbetrag);
+            System.out.print("Eingabe (mind. 5Ct, höchstens 2 Euro): ");
+            eingeworfeneMünze = tastatur.nextDouble();
+            eingezahlterGesamtbetrag += eingeworfeneMünze;
         }
-
-       // Fahrscheinausgabe
-       // -----------------
+        rückgabebetrag = eingezahlterGesamtbetrag - zuZahlenderBetrag;
+        return rückgabebetrag;
+    }
+    public static void fahrscheinausgabe() {
         System.out.println("\nFahrschein wird ausgegeben");
         for (int i = 0; i < 8; i++)
         {
@@ -47,49 +70,52 @@ class Fahrkartenautomat
 		}
         }
         System.out.println("\n\n");
+    }
 
-       // Rückgeldberechnung und -Ausgabe
-       // -------------------------------
-       rückgabebetrag = eingezahlterGesamtbetrag - zuZahlenderBetrag;
-       if(rückgabebetrag > 0.0)
-       {
-    	   System.out.printf("Der Rückgabebetrag in Höhe von %.2f Euro\n", rückgabebetrag);
-    	   System.out.println("wird in folgenden Münzen ausgezahlt:");
+    public static void rückgeldberechnung(double rückgabebetrag) {
 
-           while(rückgabebetrag >= 2.0) // 2 EURO-Münzen
-           {
-        	  System.out.println("2 EURO");
-	          rückgabebetrag -= 2.0;
-           }
-           while(rückgabebetrag >= 1.0) // 1 EURO-Münzen
-           {
-        	  System.out.println("1 EURO");
-	          rückgabebetrag -= 1.0;
-           }
-           while(rückgabebetrag >= 0.5) // 50 CENT-Münzen
-           {
-        	  System.out.println("50 CENT");
-	          rückgabebetrag -= 0.5;
-           }
-           while(rückgabebetrag >= 0.2) // 20 CENT-Münzen
-           {
-        	  System.out.println("20 CENT");
- 	          rückgabebetrag -= 0.2;
-           }
-           while(rückgabebetrag >= 0.1) // 10 CENT-Münzen
-           {
-        	  System.out.println("10 CENT");
-	          rückgabebetrag -= 0.1;
-           }
-           while(rückgabebetrag >= 0.05)// 5 CENT-Münzen
-           {
-        	  System.out.println("5 CENT");
- 	          rückgabebetrag -= 0.05;
-           }
-       }
+        if(rückgabebetrag > 0.0)
+        {
+            System.out.printf("Der Rückgabebetrag in Höhe von %.2f Euro\n", rückgabebetrag);
+            System.out.println("wird in folgenden Münzen ausgezahlt:");
 
-       System.out.println("\nVergessen Sie nicht, den Fahrschein\n"+
-                          "vor Fahrtantritt entwerten zu lassen!\n"+
-                          "Wir wünschen Ihnen eine gute Fahrt.");
+            while(rückgabebetrag >= 2.0) // 2 EURO-Münzen
+            {
+                System.out.println("2 EURO");
+                rückgabebetrag -= 2.0;
+            }
+            while(rückgabebetrag >= 1.0) // 1 EURO-Münzen
+            {
+                System.out.println("1 EURO");
+                rückgabebetrag -= 1.0;
+            }
+            while(rückgabebetrag >= 0.5) // 50 CENT-Münzen
+            {
+                System.out.println("50 CENT");
+                rückgabebetrag -= 0.5;
+            }
+            while(rückgabebetrag >= 0.2) // 20 CENT-Münzen
+            {
+                System.out.println("20 CENT");
+                rückgabebetrag -= 0.2;
+            }
+            while(rückgabebetrag >= 0.1) // 10 CENT-Münzen
+            {
+                System.out.println("10 CENT");
+                rückgabebetrag -= 0.1;
+            }
+            while(rückgabebetrag >= 0.05)// 5 CENT-Münzen
+            {
+                System.out.println("5 CENT");
+                rückgabebetrag -= 0.05;
+            }
+        }
+
+    }
+
+    public static void guteFahrt() {
+        System.out.println("\nVergessen Sie nicht, den Fahrschein\n"+
+                            "vor Fahrtantritt entwerten zu lassen!\n"+
+                            "Wir wünschen Ihnen eine gute Fahrt.");
     }
 }
