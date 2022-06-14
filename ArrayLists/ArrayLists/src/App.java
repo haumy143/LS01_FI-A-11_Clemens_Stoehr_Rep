a6fd289f89309f808f51130e984fb89bdb6971a9
haumy143 <66621176+haumy143@users.noreply.github.com>
2022-06-14 11:50:46 +0200

    finished ArrayLists

--- ArrayLists/ArrayLists/src/App.java
+++ ArrayLists/ArrayLists/src/App.java
@@ -6,7 +6,7 @@ public class App {
         ArrayList<Integer> myList = new ArrayList<Integer>();
         Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < myList.size(); i++) {
+        for (int i = 0; i < 20; i++) {
             myList.add(i, getRandomInt(9));
         }
 
@@ -14,12 +14,36 @@ public class App {
             System.out.printf("myList %2d : %3d\n", i, myList.get(i));
         } 
 
-        System.out.println("Eine Zahl 1 - 9: ");
+        System.out.printf("Eine Zahl 1 - 9: ");
+        int Zahl = scanner.nextInt();
+        int counter = 0;
+        
+        for (int i = 0; i < myList.size(); i++) {
+            if (myList.get(i) == Zahl) {
+                counter++;
+                myList.remove(i);
+            }
+        }
 
+        System.out.println("Die Zahl " + Zahl + " kommt " + counter + " mal vor");
+
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.printf("myList %2d : %3d\n", i, myList.get(i));
+        } 
+
+        for (int i = 0; i < myList.size(); i++) {
+            if (myList.get(i) == 5) {
+                myList.add(i++ , 0);
+            }
+        }
+
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.printf("myList %2d : %3d\n", i, myList.get(i));
+        } 
     }
 
-    
     public static int getRandomInt(int max) {
         return(int) Math.floor(Math.random() * max);
     }
+
 }
