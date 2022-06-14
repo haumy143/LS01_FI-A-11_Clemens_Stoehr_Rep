47c7de0e54bcaf7edf5c0c17eb097e998b0a6eb5
haumy143 <66621176+haumy143@users.noreply.github.com>
2022-06-14 11:11:56 +0200

    fixed

--- ArrayLists/ArrayLists/src/App.java
+++ ArrayLists/ArrayLists/src/App.java
@@ -4,15 +4,21 @@ import java.util.ArrayList;
 public class App {
     public static void main(String[] args) throws Exception {
         ArrayList<Integer> myList = new ArrayList<Integer>();
+        Scanner scanner = new Scanner(System.in);
 
         for (int i = 0; i < myList.size(); i++) {
             myList.add(i, getRandomInt(9));
         }
 
         for (int i = 0; i < myList.size(); i++) {
-            System.out.println(myList.get(i));
+            System.out.printf("myList %2d : %3d\n", i, myList.get(i));
         } 
+
+        System.out.println("Eine Zahl 1 - 9: ");
+
     }
+
+    
     public static int getRandomInt(int max) {
         return(int) Math.floor(Math.random() * max);
     }
